8b709d34983f3be71016e5332d5313fd4208a7e6
HereIsSee <biramantvydas@gmail.com>
2024-07-27 12:33:19 +0300

    Add clean phone number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,28 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  digit_count = digit_count_in_string(phone_number)
+  if phone_number.nil? || digit_count > 11 || digit_count < 10
+    return "The given number is invalid!"
+  end
+  if digit_count == 11 && phone_number[0] != 1
+    return "The given number is invalid!"
+  end
+  if digit_count == 11 && phone_number[0] == 1
+    return remove_first_digit_in_string(phone_number)
+  end
+  phone_number
+end
+
+def digit_count_in_string(string)
+  string.chars.reduce(0) {|sum, value| sum+= 1 if value.=~ /\d/; sum}
+end
+
+def remove_first_digit_in_string(string)
+  string.sub!(/\d/, "")
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -49,12 +71,17 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = row[:zipcode]
+  phone_number = row[:homephone]
+  
+
 
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id,form_letter)
+  phone_number = clean_phone_number(phone_number)
+  
+  puts phone_number
+  # save_thank_you_letter(id,form_letter)
 end
 
 
